f6f500a2366fabda25fee5685d16e5af6e56ad23
Helio Leao <hmtleao@hotmail.com>
2024-12-26 20:08:46 -0300

    add user creation endpoint, configure database connection, and update project structure

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,27 @@
 import { Router } from "express";
+import User from "../models/User";
 
 const router = Router();
+
+router.post("/", async (req, res) => {
+  const { name, username, email } = req.body;
+
+  const newUser = new User({
+    name,
+    username,
+    email,
+  });
+
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch {
+    res.sendStatus(400);
+  }
+});
+
+router.post("/:id/picture", async (req, res) => {
+  res.json({ message: "todo picture post" });
+});
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import usersRouter from "./routes/users";
 import petsRouter from "./routes/pets";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.DATABASE_URL!);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
 const app = express();
 app.use(express.json());
-
 app.use("/users", usersRouter);
 app.use("/pets", petsRouter);
 
