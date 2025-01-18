686c34d5aa849d96fb750ca2834836d50a07f501
Helio Leao <hmtleao@hotmail.com>
2025-01-18 17:00:05 -0300

    refactor: change types in Express Request interface to any for flexibility

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,37 +1,9 @@
 declare namespace Express {
   export interface Request {
     user?: { _id: string };
-    pet?: PetDocument;
-    task?: TaskDocument;
-    petWeightRecord?: PetWeightRecord;
-    taskDoneRecord?: TaskDoneRecord;
+    pet?: any;
+    task?: any;
+    petWeightRecord?: any;
+    taskDoneRecord?: any;
   }
 }
-
-type TaskDocument = Document<unknown, {}, ITask> &
-  ITask & {
-    _id: Types.ObjectId;
-  } & {
-    __v: number;
-  };
-
-type PetDocument = Document<unknown, {}, IPetWeightRecord> &
-  IPetWeightRecord & {
-    _id: Types.ObjectId;
-  } & {
-    __v: number;
-  };
-
-type PetWeightRecord = Document<unknown, {}, IPetWeightRecord> &
-  IPetWeightRecord & {
-    _id: Types.ObjectId;
-  } & {
-    __v: number;
-  };
-
-type TaskDoneRecord = Document<unknown, {}, ITaskDoneRecord> &
-  ITaskDoneRecord & {
-    _id: Types.ObjectId;
-  } & {
-    __v: number;
-  };
